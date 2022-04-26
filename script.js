3c607f09c3c0cd8b17ac490217d413160d7676eb
tobuya <thomasobuya95@yahoo.com>
2022-04-26 13:47:13 +0300

    Fix: enable key press and display value of key
    * Change grid elements from div to button
    * Add click event to the buttons
    * Listen to all key presses and then determine the type of key pressed
    * Determine the key pressed and return value
    * Display the value returned on the display section

--- script.js
+++ script.js
@@ -1,89 +1,44 @@
-const resultArea = document.querySelector("#top");
+//First we listen to all keypresses and then determine the type of key pressed
+//We then use the data-action attribute to determine the key that is pressed
+
+const calculator = document.querySelector("#container");
+const keys = calculator.querySelectorAll(".keys");
 const display = document.querySelector(".display");
-const toClear = document.querySelector(".left");
-const toDelete = document.querySelector(".right");
-const sevenBtn = document.querySelector(".seven");
-const eightBtn = document.querySelector(".eight");
-const nineBtn = document.querySelector(".nine");
-const divideBtn = document.querySelector(".divide");
-const fourBtn = document.querySelector(".four");
-const fiveBtn = document.querySelector(".five");
-const sixBtn = document.querySelector(".six");
-const multipleBtn = document.querySelector(".multiple");
-const oneBtn = document.querySelector(".one");
-const twoBtn = document.querySelector(".two");
-const threeBtn = document.querySelector(".three");
-const minusBtn = document.querySelector(".minus");
-const periodBtn = document.querySelector(".period");
-const zeroBtn = document.querySelector(".zero");
-const equalBtn = document.querySelector(".equal");
-const plusBtn = document.querySelector(".plus");
 
-
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-
-function returnZero() {
-    const zero = `${numbers[0]}`;
-    return Number(zero);
-}
-zeroBtn.value = returnZero();
-
-function returnOne() {
-    const  one = `${numbers[1]}`;
-    return Number(one);
-}
-oneBtn.value = returnOne();
-
-function returnTwo() {
-    const two = `${numbers[2]}`;
-    return Number(two);
-}
-twoBtn.value = returnTwo();
-
-function returnThree() {
-    const three = `${numbers[3]}`;
-    return Number(three);
-}
-threeBtn.value = returnThree();
-
-function returnFour() {
-    const four = `${numbers[4]}`;
-    return Number(four);
-}
-fourBtn.value = returnFour();
-
-function returnFive() {
-    const five = `${numbers[5]}`;
-    return Number(five);
-}
-fiveBtn.value = returnFive();
-
-function returnSix() {
-    const six = `${numbers[6]}`;
-    return Number(six);
-}
-sixBtn.value = returnSix();
-
-function returnSeven() {
-    const seven = `${numbers[7]}`;
-    return Number(seven);
-}
-sevenBtn.value = returnSeven();
-
-function returnEight() {
-    const eight = `${numbers[8]}`;
-    return Number(eight);
-}
-eightBtn.value = returnEight();
-
-function returnNine() {
-    const nine = `${numbers[9]}`;
-    return Number(nine);
-}
-nineBtn.value = returnNine();
-
-
-oneBtn.addEventListener("click", () => {})
-
-console.log(typeof(returnOne()));
+Array.from(keys).forEach(key => {
+    key.addEventListener("click", e => {
+        if(e.target.matches("button")) {
+            const key = e.target;
+            const action = key.dataset.action;
+            const keyContent = key.textContent;
+            const displayedNum = display.textContent;
+            if(!action) {
+                if(displayedNum === "0"){
+                    display.textContent = keyContent;
+                }else {
+                    display.textContent = displayedNum + keyContent;
+                }
+            }
+            if(
+                action === "add" ||
+                action === "subtract" ||
+                action === "multiply" ||
+                action === "divide"
+            ) {
+                console.log("operator key!");
+            }
+            if(action === "decimal") {
+                display.textContent = displayedNum + ".";
+            }
+            if(action === "clear") {
+                console.log("clear key!");
+            }
+            if(action === "delete") {
+                console.log("delete key!");
+            }
+            if(action === "calculate") {
+                console.log("calculate key!");
+            }
+        }
+    });
+});
